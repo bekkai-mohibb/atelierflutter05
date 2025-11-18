9e2c6125adeb0321d93fd5d13f98921e8b051b4b
bekkai-mohibb <koukou0719@hotmail.com>
2025-11-18 12:07:29 -0500

    etape3qst123

---
+++ lib/page_infos.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PageInfos extends StatelessWidget {
+  const PageInfos({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
