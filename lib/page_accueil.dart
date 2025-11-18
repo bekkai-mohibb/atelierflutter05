0a40699e6b7b504b1e0329ceb113f018ffe2b5aa
bekkai-mohibb <koukou0719@hotmail.com>
2025-11-18 11:51:54 -0500

    commit1

---
+++ lib/page_accueil.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PageAccueil extends StatelessWidget {
+  const PageAccueil({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
